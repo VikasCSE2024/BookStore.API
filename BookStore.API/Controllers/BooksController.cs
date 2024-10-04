8f2e02e23146b405a4cdf6232585f2b0b34940ea
Beinglitian <beinglitian2021@gmail.com>
2024-10-04 12:56:29 +0530

    Commented the create code

--- BookStore.API/Controllers/BooksController.cs
+++ BookStore.API/Controllers/BooksController.cs
@@ -38,12 +38,12 @@ namespace BookStore.API.Controllers
             return Ok(book);
         }
 
-        [HttpPost("")]
+        /*[HttpPost("")]
         public async Task<IActionResult> AddNewBook([FromBody]BookModel bookModel)
         {
             var id = await _bookRepository.AddBookAsync(bookModel);
             return CreatedAtAction(nameof(GetBookById),new {id =id, Controller = "books"}, id);
-        }
+        }*/
 
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateBook([FromBody] BookModel bookModel, [FromRoute]int id)
